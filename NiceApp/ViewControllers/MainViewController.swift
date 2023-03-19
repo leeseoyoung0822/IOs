c3792ad849973524a3810318850a2532842cbc9e
leeseoyoung0822 <822angel@naver.com>
2023-03-19 22:54:48 +0900

    feat : 에니메이션 실행

--- NiceApp/ViewControllers/MainViewController.swift
+++ NiceApp/ViewControllers/MainViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import Lottie
 
-class MainViewController: UIViewController {
+class MainViewController : UIViewController {
     
     let titleLable:UILabel = {
         let label = UILabel()
@@ -18,18 +19,44 @@ class MainViewController: UIViewController {
         
         return label
     }()
+    
+    let animationView : AnimationView = {
+        
+        let animView = AnimationView(name : "139849-in-tempus")
+        animView.frame = CGRect(x:0, y:0, width : 400, height: 400)
+        animView.contentMode = .scaleAspectFill //크기에 맞게 이미지 확대
+        return animView
+    }()
 
     //뷰가 생성되었을때
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.addSubview(titleLable)
+        view.backgroundColor = .black
+        view.addSubview(animationView)
+        animationView.center = view.center
         
-        titleLable.translatesAutoresizingMaskIntoConstraints = false
-        titleLable.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLable.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    
+        //에니메이션 실행
+        animationView.play{ (finish) in
+            //
+            print("애니메이션이 끝났다")
+            
+            self.view.backgroundColor = .white
+            self.animationView.removeFromSuperview() //애니매이션 뷰의 상위 부모가 뷰. 뷰가 애니메이션 뷰를 품음. 슈퍼 뷰는 뷰. 뷰로 부터 애니메이션 제거.
+            
+            
+            
+            self.view.addSubview(titleLable)
+            
+            self.titleLable.translatesAutoresizingMaskIntoConstraints = false
+            self.titleLable.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            self.titleLable.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        }
         
+        
+                
     }
 
 
